06f9976e8a68fe632965d82b96c6eef3bca462e3
Joel <60098544+Joelrumex@users.noreply.github.com>
2023-12-20 22:08:09 +0100

    Implementar canvi escena en interactuar

--- Assets/Scripts/MeshInteractuable.cs
+++ Assets/Scripts/MeshInteractuable.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class MeshInteractuable : MonoBehaviour
 {
     [SerializeField] private string interactText;
     public void Interact() {
-        Debug.Log("Interact!");
+        SceneManager.LoadScene("Scenes/Minijocs/Minijoc 1");
     }
 
     public string returnInteractText(){
