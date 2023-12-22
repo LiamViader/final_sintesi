28292fd0dfea216d46cb6b929026f05cd425825b
Joel <60098544+Joelrumex@users.noreply.github.com>
2023-12-22 18:45:38 +0100

    Camera Jugador

--- Assets/Scripts/MeshInteractuable.cs
+++ Assets/Scripts/MeshInteractuable.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using Cinemachine;
 
 public class MeshInteractuable : MonoBehaviour
 {
     [SerializeField] private string interactText;
-    public virtual void Interact() {
-        SceneManager.LoadScene("Scenes/Minijocs/Minijoc 1");
+    [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+    
+    public void Interact() {
+        cinemachineVirtualCamera.m_Priority = 9;
     }
 
     public string returnInteractText(){
