37a010428197df5f206e3af720282de6d9b748f6
Laia <u1979260@campus.udg.edu>
2024-01-24 18:22:44 +0100

    minijjoc peques detalls funcionals
    j afunciona 100%

--- Assets/Scripts/AbilitarJoc.cs
+++ Assets/Scripts/AbilitarJoc.cs
@@ -4,20 +4,17 @@ using UnityEngine.Events;
     public class SimpleScript : MonoBehaviour
     {
 
-        [SerializeField] public MinijocInteractuable funciona;
         public UnityEvent abilitar;
         public UnityEvent desabilitar;
         void Start(){
         }
-        void Update()
+        public void Abilitar()
         {
-            if (funciona.getInteractuar()){
-                Debug.Log("Abilitar");
-                abilitar.Invoke();
-            }
-            else{
-                desabilitar.Invoke();
-            }
+          abilitar.Invoke();
         }
 
+        public void Desabilitar()
+        {
+            desabilitar.Invoke();      
+        }
     }
